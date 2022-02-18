3a2430d8246022cd04f7917eb16bbf20514e6aab
jleonx <jleon.x@gmail.com>
2022-02-18 11:33:14 -0500

    Agregue espacio en los titulos cuando usamos el <!-- menu 2 -->

--- landing/js/smooth.js
+++ landing/js/smooth.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
   scrollLink.click(function(e) {
     e.preventDefault();
     $('body,html').animate({
-      scrollTop: $(this.hash).offset().top
+      scrollTop: $(this.hash).offset().top - 100
     }, 1000 );
   });
   
